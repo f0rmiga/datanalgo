4a958575249f1dd3fe3b3928a9318de0142ed47a
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2023-03-28 23:34:06 -0700

    refactor: concurrent transformer (#2)
    This introduces the Transformer interface and handles transformation
    actions with or without errors. In addition, multiple transformation
    actions can be provided.
    
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

---
+++ concurrent/concurrent_transformer_test.go
@@ -0,0 +1,226 @@
+// Copyright 2023 Thulio Ferraz Assis
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package concurrent_test
+
+import (
+	"errors"
+	"strings"
+	"testing"
+
+	"github.com/f0rmiga/datanalgo/concurrent"
+)
+
+func TestTransform(t *testing.T) {
+	testCasesStringToString := []testCase[string, string]{
+		{
+			name:     "To upper case",
+			input:    []string{"a", "b", "c"},
+			actions:  []concurrent.TransformAction[string, string]{strings.ToUpper},
+			workers:  3,
+			expected: []string{"A", "B", "C"},
+		},
+		{
+			name:     "To upper case with 1 worker",
+			input:    []string{"a", "b", "c"},
+			actions:  []concurrent.TransformAction[string, string]{strings.ToUpper},
+			workers:  1,
+			expected: []string{"A", "B", "C"},
+		},
+		{
+			name:     "To upper case with 10 workers",
+			input:    []string{"a", "b", "c"},
+			actions:  []concurrent.TransformAction[string, string]{strings.ToUpper},
+			workers:  10,
+			expected: []string{"A", "B", "C"},
+		},
+	}
+
+	for _, tc := range testCasesStringToString {
+		t.Run(tc.name, runTest(tc))
+	}
+
+	testCasesStringToInt := []testCase[string, int]{
+		{
+			name:     "Length of strings",
+			input:    []string{"a", "bb", "ccc"},
+			actions:  []concurrent.TransformAction[string, int]{func(s string) int { return len(s) }},
+			workers:  3,
+			expected: []int{1, 2, 3},
+		},
+	}
+
+	for _, tc := range testCasesStringToInt {
+		t.Run(tc.name, runTest(tc))
+	}
+
+	testCasesMultipleTransformers := []testCase[string, string]{
+		{
+			name:  "Multiple transformers",
+			input: []string{"a", "b", "c"},
+			actions: []concurrent.TransformAction[string, string]{
+				strings.ToUpper,
+				strings.ToLower,
+			},
+			workers:  3,
+			expected: []string{"a", "b", "c"},
+		},
+	}
+
+	for _, tc := range testCasesMultipleTransformers {
+		t.Run(tc.name, runTest(tc))
+	}
+}
+
+func TestTransformWithError(t *testing.T) {
+	errorFunc := errorFunc[string] // Workaround for type inference.
+	testCasesStringToString := []testCase[string, string]{
+		{
+			name:      "To upper case",
+			input:     []string{"a", "b", "c"},
+			actions:   []concurrent.TransformActionWithError[string, string]{upperCase},
+			workers:   3,
+			expected:  []string{"A", "B", "C"},
+			expectErr: false,
+		},
+		{
+			name:      "To upper case with 1 worker",
+			input:     []string{"a", "b", "c"},
+			actions:   []concurrent.TransformActionWithError[string, string]{upperCase},
+			workers:   1,
+			expected:  []string{"A", "B", "C"},
+			expectErr: false,
+		},
+		{
+			name:      "To upper case with 10 workers",
+			input:     []string{"a", "b", "c"},
+			actions:   []concurrent.TransformActionWithError[string, string]{upperCase},
+			workers:   10,
+			expected:  []string{"A", "B", "C"},
+			expectErr: false,
+		},
+		{
+			name:      "Error handling",
+			input:     []string{"a", "b", "c"},
+			actions:   []concurrent.TransformActionWithError[string, string]{errorFunc},
+			workers:   3,
+			expected:  []string{"", "", ""},
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range testCasesStringToString {
+		t.Run(tc.name, runTestWithError(tc))
+	}
+
+	testCasesStringToInt := []testCase[string, int]{
+		{
+			name:      "Length of strings",
+			input:     []string{"a", "bb", "ccc"},
+			actions:   []concurrent.TransformActionWithError[string, int]{length},
+			workers:   3,
+			expected:  []int{1, 2, 3},
+			expectErr: false,
+		},
+	}
+
+	for _, tc := range testCasesStringToInt {
+		t.Run(tc.name, runTestWithError(tc))
+	}
+
+	testCasesMultipleTransformers := []testCase[string, string]{
+		{
+			name:  "Multiple transformers",
+			input: []string{"a", "b", "c"},
+			actions: []concurrent.TransformActionWithError[string, string]{
+				upperCase,
+				lowerCase,
+			},
+			workers:   3,
+			expected:  []string{"a", "b", "c"},
+			expectErr: false,
+		},
+	}
+
+	for _, tc := range testCasesMultipleTransformers {
+		t.Run(tc.name, runTestWithError(tc))
+	}
+}
+
+func runTest[Input any, Output comparable](tc testCase[Input, Output]) func(t *testing.T) {
+	return func(t *testing.T) {
+		transformer := concurrent.NewTransformer[Input, Output](tc.workers)
+		actions := tc.actions.([]concurrent.TransformAction[Input, Output])
+		result := transformer.Transform(tc.input, actions...)
+		assertTestCase(t, tc, result)
+	}
+}
+
+func runTestWithError[Input any, Output comparable](tc testCase[Input, Output]) func(t *testing.T) {
+	return func(t *testing.T) {
+		transformer := concurrent.NewTransformer[Input, Output](tc.workers)
+		actions := tc.actions.([]concurrent.TransformActionWithError[Input, Output])
+		result, err := transformer.TransformWithError(tc.input, actions...)
+		if tc.expectErr {
+			if err == nil {
+				t.Errorf("Expected error, but got none")
+			}
+		} else {
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			assertTestCase(t, tc, result)
+		}
+	}
+}
+
+func assertTestCase[Input any, Output comparable](t *testing.T, tc testCase[Input, Output], result []Output) {
+	if len(result) != len(tc.expected) {
+		t.Errorf("Expected length %d, got %d", len(tc.expected), len(result))
+	}
+	for i, item := range result {
+		if item != tc.expected[i] {
+			t.Errorf("Expected item %v at index %d, got %v", tc.expected[i], i, item)
+		}
+	}
+}
+
+type testCase[Input any, Output any] struct {
+	name      string
+	input     []Input
+	actions   any
+	workers   int
+	expected  []Output
+	expectErr bool
+}
+
+// Test helper: error function
+func errorFunc[T any](item T) (T, error) {
+	return item, errors.New("error")
+}
+
+// Test helper: strings.ToUpper
+func upperCase(item string) (string, error) {
+	return strings.ToUpper(item), nil
+}
+
+// Test helper: strings.ToLower
+func lowerCase(item string) (string, error) {
+	return strings.ToLower(item), nil
+}
+
+// Test helper: length function for strings
+func length(item string) (int, error) {
+	return len(item), nil
+}
